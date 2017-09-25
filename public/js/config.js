d319c70319fcda19cf0fb9b12fa60ef0a67ac0df
tcyaaa <768953832@.qq.com>
2017-09-25 09:31:03 +0800

    添加富文本功能

--- public/js/config.js
+++ public/js/config.js
@@ -7,11 +7,17 @@ require.config({
     bootstrap : 'bootstrap/js/bootstrap.min',
     datepicker : 'bootstrap-datepicker/js/bootstrap-datepicker',
     language : 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+    validate : 'validate/jquery-validate',
+    form : 'jquery-form/jquery.form',
+    uploadify : 'uploadify/jquery.uploadify.min',
+    region : 'jquery-region/jquery.region',
+    ckeditor:'ckeditor/ckeditor',
     util : '../js/util',
     common : '../js/common',
     login : '../js/login',
     teacherlist : '../js/teacher-list',
-    teacheradd : '../js/teacher-add'
+    teacheradd : '../js/teacher-add',
+    settings : '../js/settings'
   },
   shim : {
     bootstrap : {
@@ -19,6 +25,16 @@ require.config({
     },
     language : {
       deps : ['jquery','datepicker']
+    },
+    validate : {
+      deps : ['jquery']
+    },
+    uploadify : {
+      deps : ['jquery']
+    },
+    ckeditor:{
+      exports:'CKEDITOR'
     }
+
   }
 });
