9af01da035b41e6d2488ca9d11a6feff59c47748
tcyaaa <768953832@.qq.com>
2017-09-25 09:39:51 +0800

    添加富文本功能

---
+++ public/js/settings.js
@@ -0,0 +1,37 @@
+define(['jquery','template','ckeditor','uploadify','region', 'datepicker', 'language'],function($,template,CKEDITOR){
+  // 调用接口获取个人信息
+  $.ajax({
+    type : 'get',
+    url : '/api/teacher/profile',
+    dataType : 'json',
+    success : function(data) {
+      // 解析数据，渲染页面
+      var html = template('settingsTpl', data.result);
+      $('#settingsInfo').html(html);
+      // 处理头像上传
+      $('#upfile').uploadify({
+        width: 120,
+        height: 120,
+        buttonText: '',
+        itemTemplate: '<span></span>',
+        swf: '/public/assets/uploadify/uploadify.swf',
+        uploader: '/api/uploader/avatar',
+        fileObjName: 'tc_avatar',
+        onUploadSuccess: function (a, b) {
+          var obj = JSON.parse(b);
+          $('.preview img').attr('src', obj.result.path);
+        }
+      });
+      // 处理省市县三级联动
+      $('#pcd').region({
+        url: '/public/assets/jquery-region/region.json'
+      });
+      //处理富文本
+      CKEDITOR.replace('editor', {
+        toolbarGroups: [
+          {name: 'clipboard', groups: ['clipboard', 'undo']},
+          {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']}
+          ]
+      });
+    }});
+});
